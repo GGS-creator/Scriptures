d5e588e453f5986a5e1c3a794abc9d9b8b595ddb
GGS-creator <gagansaralaya@gmail.com>
2026-05-09 18:54:02 +0530

    rete_limit issue

--- Backend/main.py
+++ Backend/main.py
@@ -6,7 +6,7 @@ from pydantic import BaseModel
 import random
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
-from slowapi import Limiter
+from slowapi import Limiter, _rate_limit_exceeded_handler 
 from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
 
